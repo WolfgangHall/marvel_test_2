8a3279488f1fab4f605d2bfa607457400d29bbc4
shreyp <shreyansap@gmail.com>
2016-04-02 09:43:15 -0400

    modified user model, and added password hashing

--- client/models/userModel.js
+++ client/models/userModel.js
@@ -10,16 +10,42 @@ var Schema = mongoose.Schema;
     console.log('Mongoose connection successful.');
   });
 
-  var UserSchema = new Schema({
+  var userSchema = new Schema({
     created: {
       type: Date,
       default: Date.now()
     },
     username : {
       type: String,
-      trim: true
-    }
+      trim: true,
+      unique: true
+    },
+    firstName: String,
+    lastName: String,
+    email: String,
+    password: String
   });
 
+userSchema.pre("save", function(next){
+  var user = this;
+
+  //will only hash the password if it is new or has been modified
+  if(!user.isModified("password")) return next();
+
+  //salt generation
+  bcrypt.genSalt(10, function(err, salt) {
+    if (err) return next(err);
+
+    //hash password with salt
+    bcrypt.hash(user.password, salt, function(err, hash) {
+
+      //override cleartext password with hashed password
+      user.password = hash'
+      next();
+    });
+  });
+});
+
 var User = mongoose.model('User', UserSchema);
+
 module.exports = User;
