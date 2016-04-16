e85675aa66d1a0af1db44c559ab31fecbe2ed9bf
WolfgangHall <johnalexander.w.hall@gmail.com>
2016-04-15 22:18:42 -0400

    remapped rooms page with ng repeat and added filter search

--- client/js/controllers/roomController.js
+++ client/js/controllers/roomController.js
@@ -1,14 +1,15 @@
 angular.module('chatApp')
   .controller('roomController', function($scope){
 
-    
+    $scope.rooms = [
+      {'name': 'Biology', 'description': 'Discuss the wonders of Bio'},
+      {'name': 'Literature', 'description': 'From Steinbeck to Shakespeare'},
+      {'name': 'Dark Souls 3', 'description': 'Discuss gameplay from DS3'},
+      {'name': 'The Life of Pablo', 'description': "Discuss Kanye West\'s the Life of Pablo"},
+      {'name': 'Daredevil', 'description': 'Discuss the Netflix original Daredevil'},
+      {'name': 'React JS', 'description': 'Discuss ReactJS projects'}
+    ];
 
-    $scope.roomName = 'Biology';
-    $scope.roomName2 = 'Chemistry';
-    $scope.roomName3 = 'Physics';
-    $scope.roomName4 = 'Literature';
-    $scope.roomName5 = 'The Life of Pablo';
-    $scope.roomName6 = 'Javascript';
 
 
 
