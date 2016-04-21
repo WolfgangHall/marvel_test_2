7fe82154bbdc38032a495b0a1f543a1d733599f4
unknown <shreyansap@gmail.com>
2016-04-20 20:24:58 -0400

    fixed some room stuff

--- client/js/controllers/roomController.js
+++ client/js/controllers/roomController.js
@@ -1,30 +1,5 @@
 angular.module('chatApp').controller('roomController', ['$scope','$http','$location', '$cookies', function($scope, $http, $location, $cookies){
 
-    // $scope.rooms = [
-    //   {'name': 'Biology', 'description': 'Discuss the wonders of Bio'},
-    //   {'name': 'Literature', 'description': 'From Steinbeck to Shakespeare'},
-    //   {'name': 'Dark Souls 3', 'description': 'Discuss gameplay from DS3'},
-    //   {'name': 'The Life of Pablo', 'description': "Discuss Kanye West\'s the Life of Pablo"},
-    //   {'name': 'Daredevil', 'description': 'Discuss the Netflix original Daredevil'},
-    //   {'name': 'React JS', 'description': 'Discuss ReactJS projects'},
-    //   {'name': 'Biology', 'description': 'Discuss the wonders of Bio'},
-    //   {'name': 'Literature', 'description': 'From Steinbeck to Shakespeare'},
-    //   {'name': 'Dark Souls 3', 'description': 'Discuss gameplay from DS3'},
-    //   {'name': 'The Life of Pablo', 'description': "Discuss Kanye West\'s the Life of Pablo"},
-    //   {'name': 'Daredevil', 'description': 'Discuss the Netflix original Daredevil'},
-    //   {'name': 'React JS', 'description': 'Discuss ReactJS projects'},
-    //   {'name': 'React JS', 'description': 'Discuss ReactJS projects'},
-    //   {'name': 'React JS', 'description': 'Discuss ReactJS projects'},
-    //   {'name': 'React JS', 'description': 'Discuss ReactJS projects'},
-    //   {'name': 'React JS', 'description': 'Discuss ReactJS projects'},
-    //   {'name': 'React JS', 'description': 'Discuss ReactJS projects'},
-    //   {'name': 'React JS', 'description': 'Discuss ReactJS projects'},
-    //   {'name': 'React JS', 'description': 'Discuss ReactJS projects'},
-    //   {'name': 'React JS', 'description': 'Discuss ReactJS projects'},
-    //   {'name': 'React JS', 'description': 'Discuss ReactJS projects'}
-    // ];
-
-
   $scope.getRooms = function(){
       $http.get('/rooms').then(function(response){
         $scope.roomCount = response.data.length;
